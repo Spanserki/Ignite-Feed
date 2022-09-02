989876be7a6c029d0f0b600c8734f2727f79ac63
Guilherme da Rocha Spanserki <guilherme.spanserki@cas.ilumisol.com.br>
2022-09-02 16:15:08 -0300

    Concluindo a estilização do Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,6 +3,8 @@ import styles from './Post.module.css'
 export function Post() {
     return (
         <article className={styles.post}>
+
+            {/* Cabeçalho com os dados de quem publicou */}
             <header>
                 <div className={styles.author}>
                     <img
@@ -16,20 +18,30 @@ export function Post() {
                      </div>
                 </div>
 
-                <time title='02 de Setembro as 10h58' dateTime='2022-05-08 08:13:26'>Publicado a 1h</time>
+                <time className={styles.time} title='02 de Setembro as 10h58' dateTime='2022-05-08 08:13:26'>Publicado a 1h</time>
             </header>
 
+            {/* Listagem da publicação */}
             <div className={styles.content}>
                 
                 <p>Fala galeraa 👋 </p>
 
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀 </p>
+                <p>Acabei de subir mais um projeto no meu portifolio É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀 </p>
 
                 <p>👉 <a href="">jane.design/doctorcare</a> </p>
 
                 <p> <a href="">#novoprojeto #nlw #rocketseat</a></p>
                 
             </div>
+
+            {/* Sessão comentarios */}
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea placeholder='Deixe seu comentario'></textarea>
+
+                <button type='submit'>Publicar</button>
+            </form>
         </article>
     )
 }
